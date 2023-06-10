d46f4a535e5078c1e9ff5a23c45c23f5c7e525ea
sumitt806 <sumitt806@gmail.com>
2023-06-11 05:24:51 +0530

    Mobile View added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 export const site_email = "info@example.com";
 export const site_phone = "9876543210";
+export const site_facebook = "https://facebook.com";
+export const site_twitter = "https://twitter.com";
+export const site_youtube = "https://youtube.com";
+export const site_instagram = "https://instagram.com";
+export const site_linkedin = "https://linkedin.com";
